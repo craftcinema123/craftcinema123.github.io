15678a23e361f52773a477b974beb19d8c2285b7
craftcinema6557 <149257456+craftcinema123@users.noreply.github.com>
2024-07-27 22:49:18 +0200

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function image() {
-    document.getElementById("overlay").style.display = "block";
-    document.getElementsByClassName("container").style = "filter: blur(8px); -webkit-filter: blur(8px);"
-};
